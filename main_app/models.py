af51d257fdd49824b8716553e1d383586c02d2bc
skullington4 <ksully121888@gmail.com>
2023-03-21 16:13:31 -0400

    Completed added comments, displaying comments per post.

--- main_app/models.py
+++ main_app/models.py
@@ -10,10 +10,6 @@ STATUS = (
     ('c', 'Complete')
 )
 
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.CharField(max_length=500)
-    timestamp = models.DateTimeField(default=datetime.now)
 
 
 class Post(models.Model):
@@ -26,21 +22,32 @@ class Post(models.Model):
         default=STATUS[0][0]
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comments = models.ManyToManyField(Comment)
 
 
 # Use https://git.generalassemb.ly/SEIR-01-23/student_resources/blob/main/unit_3/week_2/day_5/lessons/django_authentication.md
 # part 8 to associate user with post
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.title
     
     def get_absolute_url(self):
         return reverse('seekhelp')
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_business = models.BooleanField()
 
     
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(default=datetime.now)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+    def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_description_display()} on {self.timestamp}"
+  
+    class Meta:
+        ordering = ['-timestamp']
