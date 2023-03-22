10500aa5863bc1f0d7c9f70fbacd8c31c8cb41cb
skullington4 <ksully121888@gmail.com>
2023-03-21 21:27:06 -0400

    Fixed the home screen to display only your posts and it doesnt break when not logged in

--- main_app/views.py
+++ main_app/views.py
@@ -11,7 +11,6 @@ from .forms import CommentForm
 def home(request):
   # Only want home to have  a filter for posts
   # Need to comment out line 14 and 16 if you are not logged in
-  print(request.user)
   if (request.user.is_authenticated):
     posts = Post.objects.filter(user=request.user)
   else:
